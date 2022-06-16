8545d8e9d9323e30f9dae5747b356dad776d1ba6
msenor <msenor@asu.edu>
2022-06-16 13:37:31 -0700

    Create a HomePage component

---
+++ frontend/src/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const HomePage = () => (
+  <div>
+    <div>
+      <h2>Unanswered Questions</h2>
+      <button>Ask a Question</button>
+    </div>
+  </div>
+);
