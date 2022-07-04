c5bab79f0c9042ea869496ed2b8d1a8d2856a545
msenor <msenor@asu.edu>
2022-07-03 23:30:59 -0700

    Handle routes not found

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,19 +3,32 @@ import { css } from '@emotion/react';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
 import { fontFamily, fontSize, gray2 } from './Styles';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AskPage } from './AskPage';
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
+import { NotFoundPage } from './NotFoundPage';
 
 function App() {
   return (
-    <div
-      css={css`
-        font-family: ${fontFamily};
-        font-size: ${fontSize};
-        color: ${gray2};
-      `}
-    >
-      <Header />
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          color: ${gray2};
+        `}
+      >
+        <Header />
+        <Routes>
+          <Route path="" element={<HomePage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="ask" element={<AskPage />} />
+          <Route path="signin" element={<SignInPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
