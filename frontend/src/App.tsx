1d162e7d9ec8c670a7931e8efc75df6b75fd7ceb
msenor <msenor@starbucks.com>
2022-07-03 19:24:39 -0700

    Install and use emotion for styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import styles from './App.module.css';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
 
 function App() {
   return (
-    <div className={styles.container}>
+    <div
+      css={css`
+        font-family: 'Segoe UI', 'Helvetica Neue', sans-serif;
+        font-size: 16px;
+        color: #5c5a5a;
+      `}
+    >
       <Header />
       <HomePage />
     </div>
