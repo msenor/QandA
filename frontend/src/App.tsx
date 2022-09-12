0517e4606dbee1e91160141e70f656bf38ffd534
msenor <msenor@starbucks.com>
2022-09-12 13:00:29 -0700

    Reset to start of Chapter 3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,34 +1,25 @@
-/** @jsxImportSource @emotion/react */
-import { css } from '@emotion/react';
-import { Header } from './Header';
-import { HomePage } from './HomePage';
-import { fontFamily, fontSize, gray2 } from './Styles';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AskPage } from './AskPage';
-import { SearchPage } from './SearchPage';
-import { SignInPage } from './SignInPage';
-import { NotFoundPage } from './NotFoundPage';
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div
-        css={css`
-          font-family: ${fontFamily};
-          font-size: ${fontSize};
-          color: ${gray2};
-        `}
-      >
-        <Header />
-        <Routes>
-          <Route path="" element={<HomePage />} />
-          <Route path="search" element={<SearchPage />} />
-          <Route path="ask" element={<AskPage />} />
-          <Route path="signin" element={<SignInPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload. I also smell farts.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
