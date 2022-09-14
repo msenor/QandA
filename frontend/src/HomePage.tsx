c620a68676b317f51e26da79dc46b1681ad4d018
msenor <msenor@starbucks.com>
2022-09-14 12:18:52 -0700

    Implement component state in HomePage

--- frontend/src/HomePage.tsx
+++ frontend/src/HomePage.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import { QuestionList } from './QuestionList';
-import { getUnansweredQuestions } from './QuestionsData';
+import { getUnansweredQuestions, QuestionData } from './QuestionsData';
 import { PageTitle } from './PageTitle';
 import { Page } from './Page';
 
-export const HomePage = () => (
-  <Page>
-    <div>
-      <PageTitle>Unanswered Questions</PageTitle>
-      <button>Ask a Question</button>
-    </div>
-    <QuestionList data={getUnansweredQuestions()} />
-  </Page>
-);
+export const HomePage = () => {
+  const [questions, setQuestions] = React.useState<QuestionData[]>([]);
+  const [questionsLoading, setQuestionsLoading] = React.useState(true);
+  React.useEffect(() => {
+    const doGetUnansweredQuestions = async () => {
+      const unansweredQuestions = await getUnansweredQuestions();
+      setQuestions(unansweredQuestions);
+      setQuestionsLoading(false);
+    };
+    doGetUnansweredQuestions();
+  }, []);
+  return (
+    <Page>
+      <div>
+        <PageTitle>Unanswered Questions</PageTitle>
+        <button>Ask a Question</button>
+      </div>
+      {questionsLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <QuestionList data={questions || []} />
+      )}
+    </Page>
+  );
+};
