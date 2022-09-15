dcd4c74c10333f49c41666410fc37d57f8713a95
msenor <msenor@starbucks.com>
2022-09-15 11:13:53 -0700

    Add styling with Emotion

--- frontend/src/Header.tsx
+++ frontend/src/Header.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+/** @jsxImportSource @emotion/react */
+import { css } from '@emotion/react';
+import { fontFamily, fontSize, gray1, gray2, gray5 } from './Styles';
 import { UserIcon } from './Icons';
 
 export const Header = () => {
@@ -7,7 +9,21 @@ export const Header = () => {
   };
 
   return (
-    <div>
+    <div
+      css={css`
+        position: fixed;
+        box-sizing: border-box;
+        top: 0;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 10px 20px;
+        background-color: #fff;
+        border-bottom: 1px solid ${gray5};
+        box-shadow: 0 3px 7px 0 rgba(110, 112, 114, 0.21);
+      `}
+    >
       <a href="./">Q & A</a>
       <input
         type="text"
