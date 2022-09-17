a414b8e47a2fc6569d2a9807cc31b7227563fec1
msenor <msenor@starbucks.com>
2022-09-17 01:06:32 -0700

    Set up basic routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,32 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { fontFamily, fontSize, gray2 } from './Styles';
 import { Header } from './Header';
 import { HomePage } from './HomePage';
+import { AskPage } from './AskPage';
+import { SearchPage } from './SearchPage';
+import { SignInPage } from './SignInPage';
 
 function App() {
   return (
-    <div
-      css={css`
-        font-family: ${fontFamily};
-        font-size: ${fontSize};
-        color: ${gray2};
-      `}
-    >
-      <Header />
-      <HomePage />
-    </div>
+    <BrowserRouter>
+      <div
+        css={css`
+          font-family: ${fontFamily};
+          font-size: ${fontSize};
+          color: ${gray2};
+        `}
+      >
+        <Header />
+        <Routes>
+          <Route path="" element={<HomePage />} />
+          <Route path="search" element={<SearchPage />} />
+          <Route path="ask" element={<AskPage />} />
+          <Route path="signin" element={<SignInPage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
