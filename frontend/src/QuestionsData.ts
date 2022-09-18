828f981aa26130b97ae96e7fa46555007fccba2f
msenor <msenor@starbucks.com>
2022-09-17 23:18:55 -0700

    Implement search query parameters

--- frontend/src/QuestionsData.ts
+++ frontend/src/QuestionsData.ts
@@ -63,6 +63,16 @@ export const getQuestion = async (
   return results.length === 0 ? null : results[0];
 };
 
+export const searchQuestions = async (
+  criteria: string
+  ): Promise<QuestionData[]> => {
+  await wait(500);
+  return questions.filter((q) =>
+    q.title.toLowerCase().indexOf(criteria.toLowerCase()) >= 0 ||
+    q.content.toLowerCase().indexOf(criteria.toLowerCase()) >= 0
+  );
+}
+
 const wait = (ms: number): Promise<void> => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
