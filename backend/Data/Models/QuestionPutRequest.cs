2e6727b2bbaef2499cb2cd027cbfd2efb2841ff2
msenor <msenor@starbucks.com>
2022-09-19 15:49:49 -0700

    Add put and post requests to data repository

---
+++ backend/Data/Models/QuestionPutRequest.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace QandA.Data.Models
+{
+    public class QuestionPutRequest
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
