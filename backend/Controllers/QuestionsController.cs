f5469c9eddc9d27a47ded667bc462aedbfb12532
msenor <msenor@starbucks.com>
2022-09-25 22:44:28 -0700

    Add validationn to models

--- backend/Controllers/QuestionsController.cs
+++ backend/Controllers/QuestionsController.cs
@@ -65,7 +65,7 @@ namespace QandA.Controllers
         [HttpPost("answer")]
         public ActionResult<AnswerGetResponse> PostAnswer(AnswerPostRequest answerPostRequest)
         {
-            var questionExists = _dataRepository.QuestionExists(answerPostRequest.QuestionId);
+            var questionExists = _dataRepository.QuestionExists(answerPostRequest.QuestionId.Value);
             if (!questionExists)
             {
                 return NotFound();
